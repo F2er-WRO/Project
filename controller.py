f7a3f7ca717fa471d538461aece4a993bc454780
bbbrunaa <bv54732@fer.hr>
2025-05-29 20:54:55 +0200

    controller.py

--- controller.py
+++ controller.py
@@ -9,9 +9,9 @@ txt_factory.init_usb_factory()
 txt_factory.init_camera_factory()
 
 TXT_M = txt_factory.controller_factory.create_graphical_controller()
-TXT_M_I1_ultrasonic_distance_meter = txt_factory.input_factory.create_ultrasonic_distance_meter(TXT_M, 3) #lijevi senzor
-TXT_M_I2_ultrasonic_distance_meter = txt_factory.input_factory.create_ultrasonic_distance_meter(TXT_M, 2) #desni senzor
-TXT_M_I3_ultrasonic_distance_meter = txt_factory.input_factory.create_ultrasonic_distance_meter(TXT_M, 1) #senzor u sredini
+TXT_M_I1_ultrasonic_distance_meter = txt_factory.input_factory.create_ultrasonic_distance_meter(TXT_M, 1)  # sredina
+TXT_M_I5_ultrasonic_distance_meter = txt_factory.input_factory.create_ultrasonic_distance_meter(TXT_M, 5)  # desno
+TXT_M_I3_ultrasonic_distance_meter = txt_factory.input_factory.create_ultrasonic_distance_meter(TXT_M, 3)  # lijevo
 TXT_M_M1_encodermotor = txt_factory.motor_factory.create_encodermotor(TXT_M, 1)
 TXT_M_S1_servomotor = txt_factory.servomotor_factory.create_servomotor(TXT_M, 1)
 TXT_M_C1_motor_step_counter = txt_factory.counter_factory.create_encodermotor_counter(TXT_M, 1)
